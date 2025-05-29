7cfbd5f0e0927adbcd6a12c5331aedf175d03c6e
Valerio-Liuz <liuz2602@gmail.com>
2025-05-29 20:17:05 +0700

    fix ts error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from 'next';
-
 const nextConfig: NextConfig = {
-    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'static.vecteezy.com',
+                port: '',
+                pathname: '/system/resources/previews/013/901/773/original/facebook-icon-ios-facebook-social-media-logo-on-white-background-free-free-vector.jpg',
+                search: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.pinimg.com',
+                port: '',
+                pathname: '/736x/8d/ca/11/8dca11e2f18ddedc934f95cc307ebf8d.jpg',
+                search: '',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
