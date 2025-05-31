e087ee57ed804bce884a6caca8db4f47c605ef51
Valerio-Liuz <liuz2602@gmail.com>
2025-05-31 15:59:33 +0700

    fix eslint error

--- src/app/homepage/layout.tsx
+++ src/app/homepage/layout.tsx
@@ -1,6 +1,7 @@
 import { JSX } from 'react';
 import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 
 export default function RootLayout({
@@ -16,10 +17,7 @@ export default function RootLayout({
                         <Image src="https://i.pinimg.com/736x/8d/ca/11/8dca11e2f18ddedc934f95cc307ebf8d.jpg" alt="logo icon" width={80} height={80}/>
                         <p>Quizz...</p>
                     </div>
-                    <button className="border-black border-2 rounded-lg py-1 px-2 ">
-                    🏠
-                    Join Room
-                    </button>
+                    <Button className="bg-white text-black border-black border-2 hover:text-white">🏠 Join Room</Button>
                     <UserButton />
                 </header>
                 {children}
