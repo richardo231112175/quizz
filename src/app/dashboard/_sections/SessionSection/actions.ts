123fbc853cede20dd6c989ed7aef761b5df438d1
richardo231112175 <richardo.lieberio@students.mikroskil.ac.id>
2025-06-25 09:16:51 +0700

    Deleted quiz session will also delete the image in supabase storage

--- src/app/dashboard/_sections/SessionSection/actions.ts
+++ src/app/dashboard/_sections/SessionSection/actions.ts
@@ -2,6 +2,7 @@
 
 import { auth } from '@clerk/nextjs/server';
 import prisma from '@/lib/prisma';
+import supabase from '@/lib/supabase';
 
 export async function deleteSession(sessionId: number): Promise<boolean> {
     const { userId }: { userId: string | null } = await auth();
@@ -11,12 +12,16 @@ export async function deleteSession(sessionId: number): Promise<boolean> {
     }
 
     try {
-        await prisma.quizSession.delete({
+        const { image_url }: { image_url: string | null } = await prisma.quizSession.delete({
             where: {
                 id: sessionId,
                 clerk_id: userId,
             },
         });
+
+        if (image_url) {
+            await supabase.storage.from('quizz').remove([ image_url ]);
+        }
         return true;
     } catch {
         return false;
